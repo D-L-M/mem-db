9665b33d45b9f81143e1ef93e3364c48a1df16a9
David Maidment <david@dmaidment.co.uk>
2017-11-14 20:40:07 +0000

    Added basic HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,46 @@
 package main
 
 
+import (
+    "net/http"
+    "fmt"
+    "log"
+    "io/ioutil"
+)
+
 func main() {
-    // Program goes here
+
+    err := http.ListenAndServe(":9999", requestHandler{})
+    
+    log.Fatal(err)
+    
+}
+
+
+type requestHandler struct{}
+
+
+func (rh requestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+    // PUTTING a document
+    if r.Method == "PUT" {
+
+        body, err := ioutil.ReadAll(r.Body)
+
+        // Error reading the request body
+        if err != nil {
+
+            fmt.Fprintf(w, "Could not read request body")
+
+        // Request body received
+        } else {
+            
+            fmt.Fprintf(w, "PUT document to %s: %s\n", r.URL.Path, body)
+
+        }
+
+    }
+
+    
+
 }
