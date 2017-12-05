74eb2fbb87c3d196ccf9b03bf3f6774e3dbe7e3c
David Maidment <david@dmaidment.co.uk>
2017-12-05 20:33:59 +0000

    Moved the server initialisation code out of the main namespace

--- src/main.go
+++ src/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"net"
-	"net/http"
-
 	"./auth"
-	"./output"
 	"./routing"
+	"./server"
 	"./store"
-	"./types"
 )
 
 // Entry point
@@ -19,10 +13,8 @@ func main() {
 	// Register HTTP routes
 	routing.RegisterRoutes()
 
-	// Set up a HTTP server
-	requestHandler := &RequestHandler{}
-
-	requestHandler.Start()
+	// Set up a server
+	server.InitTCP()
 
 	// Reindex all documents previously flushed to disk
 	store.IndexFromDisk()
@@ -35,55 +27,5 @@ func main() {
 
 	// Block execution so the asynchronous code can handle requests
 	select {}
-
-}
-
-// RequestHandler defines the HTTP request handler
-type RequestHandler struct{}
-
-// Start is the TCP server initialiser
-func (requestHandler *RequestHandler) Start() {
-
-	http.HandleFunc("/", requestHandler.dispatcher)
-
-	server := &http.Server{}
-	listener, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.IPv4(0, 0, 0, 0), Port: 9999})
-
-	if err != nil {
-		log.Fatal("Error creating TCP listener")
-	}
-
-	go server.Serve(listener)
-
-}
-
-// Handle incoming requests and route to the appropriate package
-func (requestHandler *RequestHandler) dispatcher(response http.ResponseWriter, request *http.Request) {
-
-	body, err := ioutil.ReadAll(request.Body)
-
-	if err != nil {
-
-		output.WriteJSONErrorMessage(&response, "", "Could not read request body", http.StatusBadRequest)
-
-	} else {
-
-		if auth.CheckBasic(request) == false {
-
-			output.WriteJSONResponse(&response, types.JSONDocument{"success": false, "message": "Not authorised"}, http.StatusUnauthorized)
-
-		} else {
-
-			method := request.Method
-			path := request.URL.Path[:]
-			id := request.URL.Path[1:]
-
-			if routing.Dispatch(&response, method, path, id, &body) == false {
-				output.WriteJSONResponse(&response, types.JSONDocument{"success": false, "message": "Unknown request"}, http.StatusBadRequest)
-			}
-
-		}
-
-	}
 
 }
---
+++ src/server/tcp.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+
+	"../auth"
+	"../output"
+	"../routing"
+	"../types"
+)
+
+// tcpRequestHandler defines the HTTP request handler
+type tcpRequestHandler struct{}
+
+// Start is the TCP server initialiser
+func (requestHandler *tcpRequestHandler) Start() {
+
+	http.HandleFunc("/", requestHandler.dispatcher)
+
+	server := &http.Server{}
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.IPv4(0, 0, 0, 0), Port: 9999})
+
+	if err != nil {
+		log.Fatal("Error creating TCP listener")
+	}
+
+	go server.Serve(listener)
+
+}
+
+// Handle incoming requests and route to the appropriate package
+func (requestHandler *tcpRequestHandler) dispatcher(response http.ResponseWriter, request *http.Request) {
+
+	body, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+
+		output.WriteJSONErrorMessage(&response, "", "Could not read request body", http.StatusBadRequest)
+
+	} else {
+
+		if auth.CheckBasic(request) == false {
+
+			output.WriteJSONResponse(&response, types.JSONDocument{"success": false, "message": "Not authorised"}, http.StatusUnauthorized)
+
+		} else {
+
+			method := request.Method
+			path := request.URL.Path[:]
+			id := request.URL.Path[1:]
+
+			if routing.Dispatch(&response, method, path, id, &body) == false {
+				output.WriteJSONResponse(&response, types.JSONDocument{"success": false, "message": "Unknown request"}, http.StatusBadRequest)
+			}
+
+		}
+
+	}
+
+}
+
+// InitTCP initialises the TCP server
+func InitTCP() {
+
+	requestHandler := &tcpRequestHandler{}
+
+	requestHandler.Start()
+
+}
