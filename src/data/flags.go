c7c2becb7b25f8059f096c13f3b5d450d44efb84
David Maidment <david@dmaidment.co.uk>
2017-12-23 11:22:03 +0000

    Bugfix

--- src/data/flags.go
+++ src/data/flags.go
@@ -8,12 +8,13 @@ import (
 // GetOptions returns options from the application's input flags
 func GetOptions() (port int, peers []string) {
 
-	port = *flag.Int("port", 9999, "Port on which to listen for requests")
-	peersString := *flag.String("peers", "", "Comma-delimited list of peers serving the same database")
+	portInt := flag.Int("port", 9999, "Port on which to listen for requests")
+	peersString := flag.String("peers", "", "Comma-delimited list of peers serving the same database")
 
 	flag.Parse()
 
-	peers = strings.Split(peersString, ",")
+	port = *portInt
+	peers = strings.Split(*peersString, ",")
 
 	return
 
